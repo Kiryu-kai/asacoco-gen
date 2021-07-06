90da20bf928760b7b2e42c5ae5390ff8dc72c527
heppokofrontend <heppokofrontend@gmail.com>
2021-07-07 03:39:06 +0900

    :zap: WIP：速報リボンとテキスト仮組み

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {Layer, Text, Image, Stage} from 'react-konva';
+import baseImg from './images/base.jpg';
+import ribbonImg from './images/ribbon.png';
+import styles from './App.module.scss';
+
+const getImageObj = (img: string) => {
+  const obj = new globalThis.Image();
+
+  obj.src = img;
+
+  return obj;
+};
 
 function App() {
+  const [checked, setChecked] = useState(false);
+  const [text, setText] = useState('');
+  const Base = () => <Image image={getImageObj(baseImg)} x={0} y={0} width={1600} height={900} />;
+  const Ribbon = () => {
+    if (checked) {
+      return <Image image={getImageObj(ribbonImg)} x={44} y={100} width={892 / 3.2} height={542 / 3.2} />;
+    }
+
+    return <></>;
+  };
+    // text?: string;
+    // fontFamily?: string;
+    // fontSize?: number;
+    // fontStyle?: string;
+    // fontVariant?: string;
+    // textDecoration?: string;
+    // align?: string;
+    // verticalAlign?: string;
+    // padding?: number;
+    // lineHeight?: number;
+    // letterSpacing?: number;
+    // wrap?: string;
+    // ellipsis?: boolean;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Stage width={1600} height={900} className={styles.container}>
+        <Layer>
+          <Base />
+          <Ribbon />
+          <Text
+            text={text}
+            y={130}
+            x={70}
+            fontSize={60}
+            align="center"
+            wrap="word"
+            lineHeight={1.5}
+            verticalAlign="bottom"
+            strokeWidth={20}
+            stroke="#000"
+            lineJoin="round"
+            width={1100}
+            height={690}
+          />
+          <Text
+            text={text}
+            y={130}
+            x={70}
+            fontSize={60}
+            align="center"
+            wrap="word"
+            lineHeight={1.5}
+            verticalAlign="bottom"
+            fill="#fff"
+            stroke="#fff"
+            width={1100}
+            height={690}
+          />
+        </Layer>
+      </Stage>
+
+      <p>
+        <label>
+          <input type="checkbox" onChange={() => setChecked(!checked)} checked={checked} />
+          速報
+        </label>
+      </p>
+
+      <p>
+        <label>
+          テキスト
+          <input onChange={(e) => setText(e.target.value)} value={text} />
+        </label>
+      </p>
+    </>
   );
 }
 
