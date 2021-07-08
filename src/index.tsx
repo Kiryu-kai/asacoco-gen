fa00278b56f612b323084fd0cd8a76e271e0d1af
heppokofrontend <heppokofrontend@gmail.com>
2021-07-09 06:47:45 +0900

    :zap: 長音符を省略しない

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ ReactDOM.render(
           <p>利用に際しては、<a href="https://www.hololive.tv/terms">カバー株式会社のガイドライン</a>に準拠してください。</p>
 
           <ul>
-            <li>Twitterへ投稿するときは、公式ではないこと・これまでの企画との混同を避けるために「<a href="https://twitter.com/intent/tweet?url=https://kiryu-kai.github.io/asacoco-gen&amp;hashtags=あさココジェネレータ" target="_blank" rel="noreferrer">#あさココジェネレータ</a>」をつけて投稿してください。</li>
+            <li>Twitterへ投稿するときは、公式ではないこと・これまでの企画との混同を避けるために「<a href="https://twitter.com/intent/tweet?url=https://kiryu-kai.github.io/asacoco-gen&amp;hashtags=あさココジェネレーター" target="_blank" rel="noreferrer">#あさココジェネレーター</a>」をつけて投稿してください。</li>
             <li>本ツールを利用して被ったいかなる損害について、一切責任は負えません。自己責任でお願いします。</li>
             <li>素材提供やアイディア、利用素材の中止申請等は<a href="https://github.com/Kiryu-kai/asacoco-gen/issues">Issue</a>へお願いいたします。</li>
           </ul>
