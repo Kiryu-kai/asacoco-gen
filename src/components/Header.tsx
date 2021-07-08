c7384b8278fbbc256de0e2829a1047360fff5f64
heppokofrontend <heppokofrontend@gmail.com>
2021-07-09 06:24:08 +0900

    :zap: Dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from '../images/logo.png';
 import * as styles from './Header.module.scss';
 
-export const Header = () => (
-  <header className={styles.header}>
-    <h1 className={styles.h1}>
-      <img src={logo} alt="あさココLIVEジェネレータ" className={styles.logo} />
-    </h1>
+export const Header = () => {
+  useEffect(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.body.dataset.color = '1';
+    }
+  });
 
-    <div className={styles.ui}>
-      <p className={styles.item}>
-        <button className={styles.btn} onClick={() => document.body.dataset.color ='0'}>デフォルト</button>
-      </p>
-      <p className={styles.item}>
-        <button className={styles.btn} onClick={() => document.body.dataset.color ='1'}>ダークブルー</button>
-      </p>
-      <p className={styles.item}>
-        <button className={styles.btn} onClick={() => document.body.dataset.color ='2'}>ブラック</button>
-      </p>
-    </div>
-  </header>
-);
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.h1}>
+        <img src={logo} alt="あさココLIVEジェネレータ" className={styles.logo} />
+      </h1>
+
+      <div className={styles.ui}>
+        <p className={styles.item}>
+          <button className={styles.btn} onClick={() => document.body.dataset.color ='0'}>デフォルト</button>
+        </p>
+        <p className={styles.item}>
+          <button className={styles.btn} onClick={() => document.body.dataset.color ='1'}>ダークブルー</button>
+        </p>
+        <p className={styles.item}>
+          <button className={styles.btn} onClick={() => document.body.dataset.color ='2'}>ブラック</button>
+        </p>
+      </div>
+    </header>
+  );
+};
