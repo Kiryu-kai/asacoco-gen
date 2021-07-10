3d5bf80363cdd22ff6783bbeb1939995a22df3a9
heppokofrontend <heppokofrontend@gmail.com>
2021-07-10 14:53:12 +0900

    :sparkles: みおしゃを追加
    引用元：https://twitter.com/tsunomakiwatame/status/1315634745389142016

--- src/utils/main-img-loader.ts
+++ src/utils/main-img-loader.ts
@@ -5,6 +5,7 @@ import watameg from '../images/talents/tsunomaki-watame--gangimari.png';
 import watame03d from '../images/talents/tsunomaki-watame--03doya.png';
 import watame3ds from '../images/talents/tsunomaki-watame--3dsunglass.png';
 import watame3dg from '../images/talents/tsunomaki-watame--3dgangimari.png';
+import mio2s from '../images/talents/okami-mio--02sad.png';
 import flare02 from '../images/talents/shiranui-flare--02.png';
 import flare02ibukashige from '../images/talents/shiranui-flare--02ibukashige.png';
 import flare02smile from '../images/talents/shiranui-flare--02smile.png';
@@ -13,6 +14,7 @@ export const talents: [string, string][] = [
   ['flare - 不知火フレア（正月）', flare02],
   ['flare - 不知火フレア（正月・笑顔）', flare02smile],
   ['flare - 不知火フレア（正月・いぶかしげ）', flare02ibukashige],
+  ['mio - 大神ミオ（2019新衣装かなしい）', mio2s],
   ['watame - 角巻わため（笑顔）', watames],
   ['watame - 角巻わため（ガンギマリ）', watameg],
   ['watame - 角巻わため（こっそり）', watamek],
