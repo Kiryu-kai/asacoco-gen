810d0f2263ae5171fc38abf841eb89389f4978d8
heppokofrontend <heppokofrontend@gmail.com>
2021-07-10 11:53:31 +0900

    :sparkles: わためぇを追加
    引用元：https://twitter.com/tsunomakiwatame/status/1389271545059561473/

--- src/utils/main-img-loader.ts
+++ src/utils/main-img-loader.ts
@@ -1,15 +1,17 @@
 import watameg from '../images/talents/tsunomaki-watame--gangimari.png';
+import watame03d from '../images/talents/tsunomaki-watame--03doya.png';
 import watame3ds from '../images/talents/tsunomaki-watame--3dsunglass.png';
 import watame3dg from '../images/talents/tsunomaki-watame--3dgangimari.png';
-import flare2 from '../images/talents/shiranui-flare--02.png';
-import flare2ibukashige from '../images/talents/shiranui-flare--02ibukashige.png';
-import flare2smile from '../images/talents/shiranui-flare--02smile.png';
+import flare02 from '../images/talents/shiranui-flare--02.png';
+import flare02ibukashige from '../images/talents/shiranui-flare--02ibukashige.png';
+import flare02smile from '../images/talents/shiranui-flare--02smile.png';
 
 export const talents: [string, string][] = [
-  ['flare - 不知火フレア（正月）', flare2],
-  ['flare - 不知火フレア（正月・いぶかしげ）', flare2ibukashige],
-  ['flare - 不知火フレア（正月・笑顔）', flare2smile],
+  ['flare - 不知火フレア（正月）', flare02],
+  ['flare - 不知火フレア（正月・いぶかしげ）', flare02ibukashige],
+  ['flare - 不知火フレア（正月・笑顔）', flare02smile],
   ['watame - 角巻わため（ガンギマリ）', watameg],
+  ['watame - 角巻わため（2021新衣装ドヤ顔）', watame03d],
   ['watame - 角巻わため（3D・ガンギマリ）', watame3dg],
   ['watame - 角巻わため（3D・サングラス）', watame3ds],
 ];
