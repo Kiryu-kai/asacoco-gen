a5e96eb6514d9f458eb9812775c487e14d8fa879
heppokofrontend <heppokofrontend@gmail.com>
2021-07-10 11:26:30 +0900

    :sparkles: ふーたんを追加
    引用元：https://twitter.com/shiranuiflare/status/1212670209498075137/

--- src/utils/main-img-loader.ts
+++ src/utils/main-img-loader.ts
@@ -1,7 +1,11 @@
 import watame from '../images/talents/tsunomaki-watame--gangimari.png';
-import flare from '../images/talents/shiranui-flare--02smile.png';
+import flare2 from '../images/talents/shiranui-flare--02.png';
+import flare2ibukashige from '../images/talents/shiranui-flare--02ibukashige.png';
+import flare2smile from '../images/talents/shiranui-flare--02smile.png';
 
 export const talents: [string, string][] = [
-  ['flare - 不知火フレア（笑顔）', flare],
+  ['flare - 不知火フレア（正月）', flare2],
+  ['flare - 不知火フレア（正月・いぶかしげ）', flare2ibukashige],
+  ['flare - 不知火フレア（正月・笑顔）', flare2smile],
   ['watame - 角巻わため（ガンギマリ）', watame],
 ];
