4d4c117eed1616c3d6c0f8765c1deb8789f23cba
heppokofrontend <heppokofrontend@gmail.com>
2021-07-10 11:33:53 +0900

    :sparkles: わためぇを追加
    引用元：https://twitter.com/tsunomakiwatame/status/1289571440597983232

--- src/utils/main-img-loader.ts
+++ src/utils/main-img-loader.ts
@@ -1,5 +1,6 @@
-import watame from '../images/talents/tsunomaki-watame--gangimari.png';
+import watameg from '../images/talents/tsunomaki-watame--gangimari.png';
 import watame3ds from '../images/talents/tsunomaki-watame--3dsunglass.png';
+import watame3dg from '../images/talents/tsunomaki-watame--3dgangimari.png';
 import flare2 from '../images/talents/shiranui-flare--02.png';
 import flare2ibukashige from '../images/talents/shiranui-flare--02ibukashige.png';
 import flare2smile from '../images/talents/shiranui-flare--02smile.png';
@@ -8,6 +9,7 @@ export const talents: [string, string][] = [
   ['flare - 不知火フレア（正月）', flare2],
   ['flare - 不知火フレア（正月・いぶかしげ）', flare2ibukashige],
   ['flare - 不知火フレア（正月・笑顔）', flare2smile],
-  ['watame - 角巻わため（ガンギマリ）', watame],
+  ['watame - 角巻わため（ガンギマリ）', watameg],
+  ['watame - 角巻わため（3D・ガンギマリ）', watame3dg],
   ['watame - 角巻わため（3D・サングラス）', watame3ds],
 ];
