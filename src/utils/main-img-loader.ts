6b68f1663eedb72c8237c9939135a5854bef92ab
heppokofrontend <heppokofrontend@gmail.com>
2021-07-10 12:01:43 +0900

    :sparkles: わためぇを追加
    引用元：https://twitter.com/tsunomakiwatame/status/1315434004615589890

--- src/utils/main-img-loader.ts
+++ src/utils/main-img-loader.ts
@@ -1,4 +1,5 @@
 import watames from '../images/talents/tsunomaki-watame--smile.png';
+import watamek from '../images/talents/tsunomaki-watame--kossori.png';
 import watameg from '../images/talents/tsunomaki-watame--gangimari.png';
 import watame03d from '../images/talents/tsunomaki-watame--03doya.png';
 import watame3ds from '../images/talents/tsunomaki-watame--3dsunglass.png';
@@ -13,6 +14,7 @@ export const talents: [string, string][] = [
   ['flare - 不知火フレア（正月・笑顔）', flare02smile],
   ['watame - 角巻わため（笑顔）', watames],
   ['watame - 角巻わため（ガンギマリ）', watameg],
+  ['watame - 角巻わため（こっそり）', watamek],
   ['watame - 角巻わため（2021新衣装ドヤ顔）', watame03d],
   ['watame - 角巻わため（3D・ガンギマリ）', watame3dg],
   ['watame - 角巻わため（3D・サングラス）', watame3ds],
