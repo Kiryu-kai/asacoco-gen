10d37bdc9fd5b86836a7eca53301be36322a012c
heppokofrontend <heppokofrontend@gmail.com>
2021-07-10 15:08:19 +0900

    :heart: ちゃま！？
    引用元：https://twitter.com/shqdow_hololive/status/1288835946884956161

--- src/utils/main-img-loader.ts
+++ src/utils/main-img-loader.ts
@@ -1,3 +1,8 @@
+import flare02 from '../images/talents/shiranui-flare--02.png';
+import flare02ibukashige from '../images/talents/shiranui-flare--02ibukashige.png';
+import flare02smile from '../images/talents/shiranui-flare--02smile.png';
+import haachamag from '../images/talents/haato--gangimari.png';
+import mio2s from '../images/talents/okami-mio--02sad.png';
 import watames from '../images/talents/tsunomaki-watame--smile.png';
 import watamek from '../images/talents/tsunomaki-watame--kossori.png';
 import watamet from '../images/talents/tsunomaki-watame--tirari.png';
@@ -5,15 +10,12 @@ import watameg from '../images/talents/tsunomaki-watame--gangimari.png';
 import watame03d from '../images/talents/tsunomaki-watame--03doya.png';
 import watame3ds from '../images/talents/tsunomaki-watame--3dsunglass.png';
 import watame3dg from '../images/talents/tsunomaki-watame--3dgangimari.png';
-import mio2s from '../images/talents/okami-mio--02sad.png';
-import flare02 from '../images/talents/shiranui-flare--02.png';
-import flare02ibukashige from '../images/talents/shiranui-flare--02ibukashige.png';
-import flare02smile from '../images/talents/shiranui-flare--02smile.png';
 
 export const talents: [string, string][] = [
   ['flare - 不知火フレア（正月）', flare02],
   ['flare - 不知火フレア（正月・笑顔）', flare02smile],
   ['flare - 不知火フレア（正月・いぶかしげ）', flare02ibukashige],
+  ['haachama - 赤井はあと（ガンギマリ）', haachamag],
   ['mio - 大神ミオ（2019新衣装かなしい）', mio2s],
   ['watame - 角巻わため（笑顔）', watames],
   ['watame - 角巻わため（ガンギマリ）', watameg],
