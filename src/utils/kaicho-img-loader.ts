78638667db1cb2960baddb044be410e247908919
heppokofrontend <heppokofrontend@gmail.com>
2021-07-14 02:55:06 +0900

    :zap: 1.0との座標違いは初期のみだったため、表記を元に戻す

--- src/utils/kaicho-img-loader.ts
+++ src/utils/kaicho-img-loader.ts
@@ -4,8 +4,8 @@ import gangimariRight2 from '../images/kaicho/gangimari--right2.png';
 import smile from '../images/kaicho/smile.png';
 
 export const kaicho: [string, string][] = [
-  ['1.0 - ガンギマリ（正面）', gangimari],
-  ['3.0 - ガンギマリ（斜め右）', gangimariRight],
-  ['3.0 - ガンギマリ（斜め右2）', gangimariRight2],
-  ['3.0 - ニコニコ（正面）', smile],
+  ['ガンギマリ（正面）', gangimari],
+  ['ガンギマリ（斜め右）', gangimariRight],
+  ['ガンギマリ（斜め右2）', gangimariRight2],
+  ['ニコニコ（正面）', smile],
 ];
