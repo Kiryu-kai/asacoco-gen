213df3ba7568e83dc465639857d6ba93eabf8234
heppokofrontend <heppokofrontend@gmail.com>
2021-07-14 03:27:40 +0900

    :zap: 各種リンクを別窓に

--- src/index.tsx
+++ src/index.tsx
@@ -15,12 +15,12 @@ ReactDOM.render(
         <div className={styles.content} id="guidelines">
           <h2>お願い・免責事項</h2>
 
-          <p>利用に際しては深い愛を以って、<a href="https://www.hololive.tv/terms">カバー株式会社のガイドライン</a>に準拠してください。</p>
+          <p>利用に際しては深い愛を以って、<a href="https://www.hololive.tv/terms" target="_blank" rel="noreferrer">カバー株式会社のガイドライン</a>に準拠してください。</p>
 
           <ul>
             <li>Twitterへ投稿するときは、公式ではないこと・これまでの企画との混同を避けるために、あさココ系のハッシュタグはつけないか「<a href="https://twitter.com/intent/tweet?url=https://kiryu-kai.github.io/asacoco-gen&amp;hashtags=あさココジェネレーター" target="_blank" rel="noreferrer">#あさココジェネレーター</a>」をつけて投稿してください。</li>
             <li>本ツールを利用して被ったいかなる損害について、一切責任は負えません。自己責任でお願いします。</li>
-            <li>素材提供やアイディア、利用素材の中止申請等は<a href="https://github.com/Kiryu-kai/asacoco-gen/issues">Issue</a>へお願いいたします。</li>
+            <li>素材提供やアイディア、利用素材の中止申請等は<a href="https://github.com/Kiryu-kai/asacoco-gen/issues" target="_blank" rel="noreferrer">Issue</a>へお願いいたします。</li>
           </ul>
         </div>
       </main>
