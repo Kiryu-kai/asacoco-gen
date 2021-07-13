919f136ddcc71b9ded1bb44f4d372072fb54dc9b
heppokofrontend <heppokofrontend@gmail.com>
2021-07-14 03:43:57 +0900

    :zap: 文言追加・修正

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,8 @@ ReactDOM.render(
           <ul>
             <li>Twitterへ投稿するときは、公式ではないこと・これまでの企画との混同を避けるために、あさココ系のハッシュタグはつけないか「<a href="https://twitter.com/intent/tweet?url=https://kiryu-kai.github.io/asacoco-gen&amp;hashtags=あさココジェネレーター" target="_blank" rel="noreferrer">#あさココジェネレーター</a>」をつけて投稿してください。</li>
             <li>本ツールを利用して被ったいかなる損害について、一切責任は負えません。自己責任でお願いします。</li>
-            <li>素材提供やアイディア、利用素材の中止申請等は<a href="https://github.com/Kiryu-kai/asacoco-gen/issues" target="_blank" rel="noreferrer">Issue</a>へお願いいたします。</li>
+            <li>素材提供やアイディア、利用素材の中止申請等は<a href="https://github.com/Kiryu-kai/asacoco-gen/issues" target="_blank" rel="noreferrer">GitHub Issue</a>へお願いいたします。</li>
+            <li>今後の開発予定と作業中のタスクは<a href="https://github.com/Kiryu-kai/asacoco-gen/projects/1" target="_blank" rel="noreferrer">GitHub Projects</a>でご確認いただけます。</li>
           </ul>
         </div>
       </main>
