4bfc3c8fd4a4b4d8aa692ea766269312c6986837
Ecma Kawarabe🥒 <heppokofrontend@gmail.com>
2021-07-18 08:34:34 +0900

    :sparkles: あの人を実装

--- src/utils/kaicho-img-loader.ts
+++ src/utils/kaicho-img-loader.ts
@@ -7,6 +7,7 @@ import pekorac from '../images/kaicho/pekora--closeeyes.png';
 import v2 from '../images/kaicho/v2.png';
 import v2er from '../images/kaicho/v2--eyeright.png';
 import v2el from '../images/kaicho/v2--eyeleft.png';
+import anti from '../images/kaicho/anti.png';
 
 export const kaicho: [string, string][] = [
   ['1.0 - ガンギマリ（正面）', gangimari],
@@ -18,4 +19,5 @@ export const kaicho: [string, string][] = [
   ['2.0 - 通常（目線左）', v2el],
   ['ぺこーら', pekora],
   ['ぺこーら（目閉じ）', pekorac],
+  ['あさココアンチ', anti],
 ];
