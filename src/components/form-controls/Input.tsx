19197e8833d1c9195637d872aac6658468a5342d
heppokofrontend <heppokofrontend@gmail.com>
2023-09-24 04:46:49 +0900

    Upgrade code formatters

--- src/components/form-controls/Input.tsx
+++ src/components/form-controls/Input.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import styles from './Common.module.scss';
+import styles from '@/components/form-controls/Common.module.scss';
 
-type Component = React.FC<React.InputHTMLAttributes<HTMLInputElement> & {
-  /** アクセシビリティネーム */
-  label: string | React.ReactNode,
-}>
+type Component = React.FC<
+  React.InputHTMLAttributes<HTMLInputElement> & {
+    /** アクセシビリティネーム */
+    label: string | React.ReactNode;
+  }
+>;
 
-export const Input: Component = ({label, ...props}) => {
+export const Input: Component = ({ label, ...props }) => {
   // ? input + span 構造が使えないのでラッパーに属性を持たせます
-  const attr = (
-      props.type === 'radio' ||
-      props.type === 'checkbox'
-  )? {
-    'data-checked': props.checked,
-  } : {};
+  const attr =
+    props.type === 'radio' || props.type === 'checkbox'
+      ? {
+          'data-checked': props.checked,
+        }
+      : {};
 
   return (
     <label className={styles.host}>
-      <span className={styles.label}>
-        {label}
-      </span>
+      <span className={styles.label}>{label}</span>
       <span className={styles.wrap} {...attr} data-type={props.type}>
         <input {...props} className={styles.input} />
       </span>
--- src/components/form-controls/Select.tsx
+++ src/components/form-controls/Select.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import styles from './Common.module.scss';
+import styles from '@/components/form-controls/Common.module.scss';
 
-type Component = React.FC<React.SelectHTMLAttributes<HTMLSelectElement> & {
-  label: string
-  /** [選択項目名, 値][] */
-  options: [string, string][]
-}>
+type Component = React.FC<
+  React.SelectHTMLAttributes<HTMLSelectElement> & {
+    label: string;
+    /** [選択項目名, 値][] */
+    options: [string, string][];
+  }
+>;
 
-export const Select: Component = ({label, options, ...props}) => {
+export const Select: Component = ({ label, options, ...props }) => {
   return (
     <label className={styles.host}>
-      <span className={styles.label}>
-        {label}
-      </span>
+      <span className={styles.label}>{label}</span>
       <span className={styles.wrap}>
         <select {...props} className={styles.select}>
-          {
-            options.map(([name, value]) => (
-              <option value={value} key={name}>{name}</option>
-            ))
-          }
+          {options.map(([name, value]) => (
+            <option value={value} key={name}>
+              {name}
+            </option>
+          ))}
         </select>
       </span>
     </label>
